7406a6cc49b7a917bec809c4a6ee1ea8cfab77cb
Arfor Houwman <c7041271@uibk.ac.at>
2023-08-25 13:22:01 -0400

    Add constants

---
+++ @Constants/Constants.m
@@ -0,0 +1,20 @@
+classdef Constants
+   properties (Constant)
+      h     = 6.62606896e-34;           % Planck's constant (J*s)
+      hbar  = 1.054571817e-34;          % Reduced Planck's constant (J*s)
+      atomicMass = 1.66053906660e-27;   % Unified Atomic Mass (kg)
+      kB    = 1.380649e-23;             % Boltzmann constant (J/K)
+      eV    = 1.602176634e-19;          % electronvolt (J)
+      qE    = 1.602176634e-19;          % elementary charge (C)
+      c     = 299792458;                % Speed of light (m/s)
+      nA    = 6.02214076e23;            % Avogadro constant (1/mol)
+      mu0   = 1.25663706212e-6;         % Vacuum Magnetic Permeability (N/A^2)
+      e0    = 8.8541878128e-12;         % Vacuum Electric Permittivity (F/m)
+      muB   = 9.2740100783e-24;         % Bohr Magneton (J/T)
+      muN   = 5.050783699e-27;          % Nuclear Magneton (J/T)
+      a0    = 5.29177210903e-11;        % Bohr Radius (m)
+      D     =  1e-21*(1/299792458);     % 1 Debye (C*m)
+   end
+   methods
+   end
+end
