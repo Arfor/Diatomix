43f67e2da75bcb6cf98acd3125e28585fd31a321
Arfor Houwman <c7041271@uibk.ac.at>
2023-12-07 15:52:24 +0100

    Update rotvec2mat.m
    Included statement to return identity matrix if vector is null. Needed, otherwise returns NaN matrix

--- functions/rotvec2mat.m
+++ functions/rotvec2mat.m
@@ -3,15 +3,17 @@ function rotMat = rotvec2mat(k)
 %that you dont have to download the entire package from MATLAB
 % Uses the Rodrigues formula en.wikipedia.org/wiki/Rodrigues%27_rotation_formula#Matrix_notation
 
-assert(length(k)==3); %make sure its a 3D vector
+assert(length(k)==3,"rotvec2mat(k): k is not a 3D vector"); %make sure its a 3D vector
 t = norm(k); %get norm/angle of rotation
-r = k/t; %normalise vector
-
-K = [0, -r(3), r(2);
-     r(3), 0, -r(1);
-     -r(2), r(1), 0];
-K2 = K*K;
-
-rotMat = eye(3) + sin(t)*K + (1-cos(t))*K2;
+if t < realmin("single")
+    rotMat = eye(3);
+else
+    r = k/t; %normalise vector   
+    K = [0, -r(3), r(2);
+         r(3), 0, -r(1);
+         -r(2), r(1), 0];
+    
+    K2 = K*K;
+    rotMat = eye(3) + sin(t)*K + (1-cos(t))*K2;
 end
 
